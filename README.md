05ac0d7e65c12b33a5fcb4c9ef795215409dc6bb
sunilgoothy <sunil.goothy@tmeic.in>
2022-07-01 18:00:45 +0530

    Vue Init

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # twotter-vue3
-Learning proj for vue3. Tutorial at https://www.youtube.com/watch?v=ZqgiuPt5QZo
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    @{{ user.username }} - {{ fullName }}
+    <strong>Followers:</strong> {{ followers }}
+    <button @click="followUser">
+      Follow
+    </button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'sunohar',
+        firstName: 'Sunil',
+        lastName: 'Manohar',
+        email: 'sdfs@ssdfs.com',
+        isAdmin: true
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  methods: {
+    followUser() {
+      this.followers++
+    }
+  },
+  mounted() {
+    this.followUser();
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+}
+</style>
